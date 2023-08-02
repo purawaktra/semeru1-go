4df42fc441dca26019d8ea33e4d4e9e13dd21272
purawaktra <akbar.muhammadakbarmaulana@gmail.com>
2023-08-02 16:38:25 +0700

    chore: config env tweaks

--- utils/config.go
+++ utils/config.go
@@ -25,14 +25,10 @@ var (
 
 func InitConfig() {
 	// check app environment on env
-	env := os.Getenv("APP_ENV")
+	AppEnvironment = os.Getenv("APP_ENV")
 
 	// check for value
-	if env == "" {
-		env = "development"
-	}
-
-	if env == "development" {
+	if AppEnvironment == "" {
 		// check for config.json
 		viper.SetConfigFile(`config.json`)
 
@@ -43,12 +39,16 @@ func InitConfig() {
 			panic(err)
 		}
 
+		// check for app env
+		AppEnvironment = viper.GetString("app.environment")
+	}
+
+	if AppEnvironment == "development" {
 		// get variable for app
 		AppName = viper.GetString("app.name")
 		AppHost = viper.GetString("app.host")
 		AppCode = viper.GetString("app.code")
 		AppPort = viper.GetString("app.port")
-		AppEnvironment = viper.GetString("app.environment")
 		AppLogLevel = viper.GetString("app.log.level")
 		AppAuthUsername = viper.GetString("app.auth.username")
 		AppAuthPassword = viper.GetString("app.auth.password")
@@ -64,7 +64,7 @@ func InitConfig() {
 		return
 	}
 
-	if env == "staging" || env == "production" {
+	if AppEnvironment == "staging" || AppEnvironment == "production" {
 		// get variable for app
 		AppName = os.Getenv("APP_NAME")
 		AppPort = os.Getenv("APP_PORT")
